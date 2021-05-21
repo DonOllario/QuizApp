5a505b9aa15ad17bfd45b3e92fb2f1ecab785cec
Kristoffer <kjohansson93@hotmail.se>
2021-05-21 21:30:46 +0200

    Added pink and blue default buttons and a component with our front page logo

---
+++ QuizApp/ClientApp/src/components/DefaultButton_pink.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+import './DefaultButton_pink.css';
+
+export class DefaultButton_pink extends Component {
+
+    render() {
+        return (
+
+            <button className="button button_pink">{this.props.value}</button>
+
+        );
+    }
+}
+
+//Knappen importeras och sedan används följande kod för att implementera den:
+//<DefaultButton_pink
+//    value="Text here" />
+//
+//Value ändrar texten i knappen.
