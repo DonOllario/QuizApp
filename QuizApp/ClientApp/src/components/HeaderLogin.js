900fbd099790995ea3c38f0366d189c3c5483f12
Kristoffer <kjohansson93@hotmail.se>
2021-05-26 20:23:06 +0200

    Fixed routes, added userinfo and score

--- QuizApp/ClientApp/src/components/HeaderLogin.js
+++ QuizApp/ClientApp/src/components/HeaderLogin.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import homeImage from '../assets/KozzQuiz_logo_header.png';
 import './HeaderLogin.css';
+import { Link } from "react-router-dom";
 
 export class HeaderLogin extends Component {
     render(){
         return(
         <header className="header">
-            <a href="index.html">
+            <Link to="/">
                 <img className="header-home-img" src={homeImage} alt="home_logo"></img>
-            </a>
+            </Link>
             <button type="button" class="header-login-button align-items-center" data-toggle="modal" data-target="#logInModal"><i class="account-icon fas fa-user-circle fa-2x"></i><span >Log in</span></button>
         </header>
         );
--- QuizApp/ClientApp/src/components/HeaderProfile.js
+++ QuizApp/ClientApp/src/components/HeaderProfile.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import homeImage from '../assets/KozzQuiz_logo_header.png';
 import profileImage from '../assets/KozzQuiz_logo_header_blue.png'
 import './HeaderProfile.css';
+import { Link } from "react-router-dom";
 
 export class HeaderProfile extends Component {
     render(){
         return(
         <header className="header">
-            <a href="index.html">
+
+            <Link to="/">
                 <img className="header-home-img" src={homeImage} alt="home_logo"></img>
-            </a>
-            <button type="button" className="header-login header-login-button dropbtn"><img className="profile-img" src={profileImage} alt="blue-owl"></img>Profile</button>
+            </Link>
+
+            <Link to="/UserInfo">
+                <button type="button" className="header-login header-login-button dropbtn"><img className="profile-img" src={profileImage} alt="blue-owl"></img>Profile</button>
+            </Link>
+
         </header>
         );
     }
