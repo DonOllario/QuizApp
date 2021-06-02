710c1cc7e6a9e2ed6f1933c3a40748946c2971f7
Erkanovich <erinik90@gmail.com>
2021-06-02 11:00:05 +0200

    Drop files from .gitignore

---
+++ Data/Configurations/QuizAppUserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Configurations
+{
+    public class QuizAppUserConfiguration : IEntityTypeConfiguration<QuizAppUser>
+    {
+        public void Configure(EntityTypeBuilder<QuizAppUser> builder)
+        {
+            builder
+                .Ignore(x => x.Claims);
+        }
+    }
+}
