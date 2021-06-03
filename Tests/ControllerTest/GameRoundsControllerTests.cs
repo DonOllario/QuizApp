4cae240235cf2f1432e551cbe5e8c03b70a7e1bf
Kriistoffer <kjohansson93@hotmail.se>
2021-06-03 20:49:41 +0200

    Working on tests

---
+++ Tests/ControllerTest/GameRoundsControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Data;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using QuizApp.Controllers.RequestsAndResponses;
+
+namespace Tests.ControllerTest
+{
+    public class GameRoundsControllerTests : TestBase
+    {
+        public GameRoundsControllerTests(TestWebApplicationFactory<TestStartup> factory) : base(factory)
+        { }
+
+        [Fact]
+        public async Task Should_Return_OK_When_Starting_New_Gameround()
+        {
+            var startGameRound = new AddGameRoundRequest
+            {
+                NumberOfQuestions = 10
+            };
+            var requestDataAsJson = JsonConvert.SerializeObject(startGameRound);
+            var httpContent = new StringContent(requestDataAsJson, Encoding.UTF8, "application/json");
+
+            var response = await Client.PostAsync("gameround", httpContent);
+
+            response
+                .StatusCode
+                .Should()
+                .Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Should_Return_OK_When_Deleting_GameRound()
+        {
+            var AddGame = new GameRoundResponse
+            {
+                GameRoundId = new Guid(),
+                NumberOfQuestions = 10,
+                TimeStarted = DateTime.Now
+            };
+        }
+    }
+}
